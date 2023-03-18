e71fc2066face0b300376c8fca64d3b376e28ce0
Pavel Krivtsov <krivcov.pavel@gmail.com>
2023-03-18 22:02:05 +0300

    Add models

--- EM_TZ/NerworkService/Models.swift
+++ EM_TZ/NerworkService/Models.swift
@@ -8,18 +8,40 @@
 import Foundation
 
 // MARK: - Latest
-struct Latest: Codable {
+struct Latest: Decodable {
     let latest: [LatestElement]
 }
 
 // MARK: - LatestElement
-struct LatestElement: Codable {
+struct LatestElement: Decodable {
     let category, name: String
     let price: Int
     let imageURL: String
 
     enum CodingKeys: String, CodingKey {
         case category, name, price
+        case imageURL = "image_url"
+    }
+}
+
+// MARK: - FlashSale
+struct FlashSale: Decodable {
+    let flashSale: [FlashSaleElement]
+
+    enum CodingKeys: String, CodingKey {
+        case flashSale = "flash_sale"
+    }
+}
+
+// MARK: - FlashSaleElement
+struct FlashSaleElement: Decodable {
+    let category, name: String
+    let price: Double
+    let discount: Int
+    let imageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case category, name, price, discount
         case imageURL = "image_url"
     }
 }
