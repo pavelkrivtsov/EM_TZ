534e80ec19a0a6260d23df3e7902d719c96f5c73
Pavel Krivtsov <krivcov.pavel@gmail.com>
2023-03-24 22:32:59 +0300

    Add viewModel for home screen

---
+++ EM_TZ/ViewModels/HomeViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewModel.swift
+//  EM_TZ
+//
+//  Created by Pavel Krivtsov on 23.03.2023.
+//
+
+import UIKit
+
+final class HomeViewModel {
+    
+    private let networkService: NetworkService
+    var sections = Dynamic([Section]())
+    
+    init(networkService: NetworkService) {
+        self.networkService = networkService
+    }
+    
+    func fetchProducts() {
+        networkService.fetchAllProducts { [weak self] sections in
+            guard let self = self else { return }
+            self.sections.value = sections
+        }
+    }
+}
